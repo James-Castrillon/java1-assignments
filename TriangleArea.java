b147b602f852e274f4756d8e687a589764cd7fe1
James-Castrillon <castrillonjames13@gmail.com>
2026-04-17 23:27:21 -0600

    Implement triangle area comparison in TriangleArea
    Added functionality to read triangle dimensions and compare areas.

--- TriangleArea.java
+++ TriangleArea.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        Triangle triangle1 = new Triangle();
+        Triangle triangle2 = new Triangle();
+
+        // Read and set base and height for triangle1
+        triangle1.setBase(scnr.nextDouble());
+        triangle1.setHeight(scnr.nextDouble());
+
+        // Read and set base and height for triangle2
+        triangle2.setBase(scnr.nextDouble());
+        triangle2.setHeight(scnr.nextDouble());
+
+        System.out.println("Triangle with smaller area:");
+
+        // Determine smaller triangle and print its info
+        if (triangle1.getArea() <= triangle2.getArea()) {
+            triangle1.printInfo();
+        } else {
+            triangle2.printInfo();
+        }
+    }
+}
