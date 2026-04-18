1576b83b7135d01234f98d00e367eb2f52b35d10
James-Castrillon <castrillonjames13@gmail.com>
2026-04-17 23:20:23 -0600

    Add Triangle class with area calculation

--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,30 @@
+public class Triangle {
+    private double base;
+    private double height;
 
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+
+    public void printInfo() {
+        System.out.printf("Base: %.2f%n", base);
+        System.out.printf("Height: %.2f%n", height);
+        System.out.printf("Area: %.2f%n", getArea());
+    }
+}
